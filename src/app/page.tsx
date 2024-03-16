46fdbf9908a23306b5b5b21bb02e58c59b5704d9
Victor Hugo <victorzarbesu@gmail.com>
2024-03-16 16:46:39 -0300

    Implementando o conteúdo na page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+
+import Conteudo from "./Conteudo";
 
 export default function Home() {
   return (
     <main>
-        
+      <Conteudo texth1={"Entregue sucesso agora com o Salesforce Costumer360."}
+
+       textp={"O customer 360 é nossa súite de produtos e serviços que ajuda 98% dos clientes a alcançar ou superar suas metas ROI."}
+
+        botao1={"Faça o teste grátis"} 
+
+        botao2={"Saiba mais"}
+
+         image={"./sales.png"} alt={"imagem do produto"} />
     </main>
   );
 };
+

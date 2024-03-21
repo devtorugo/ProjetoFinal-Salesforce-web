4c55569d95e65219b3143e934cb403b9b3ac80b7
Victor Hugo <victorzarbesu@gmail.com>
2024-03-21 19:35:45 -0300

    gerando onClick do Teste grátis do conteúdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,27 @@ import Conteudo from "./conteudo";
 import Link from 'next/link';
 
 export default function Home() {
+
+  const click1 = () => {
+    console.log('Click 1');
+  }
+  const click2 = () => {
+    console.log('Click 2');
+  }
+  const click3 = () => {
+    console.log('Click 3');
+  }
   return (
     <main>
       <Conteudo texth1={"Entregue sucesso agora com o Salesforce Costumer360."}
 
-       textp={"O customer 360 é nossa súite de produtos e serviços que ajuda 98% dos clientes a alcançar ou superar suas metas ROI."}
+      textp={"O customer 360 é nossa súite de produtos e serviços que ajuda 98% dos clientes a alcançar ou superar suas metas ROI."}
 
-        botao1={"Faça o teste grátis"} 
+      botao1={"Faça o teste grátis"} onClick={click1}
 
-        botao2={"Saiba mais"}
+      botao2={"Saiba mais"} 
 
-         image={"./sales.png"} alt={"imagem do produto"} />
+      image={"./sales.png"} alt={"imagem do produto"}/>
 
          
     </main>

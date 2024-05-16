d109aa05c1f0eb37a6bacbd9e812802de706cd60
devtorugo <victorzarbesu@gmail.com>
2024-05-16 17:10:04 -0300

    Ajustando importação de clicks para p deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import ClickTracker from './Clicks/clickTracker';
+import ClickTracker from './clicks/clickTracker';
 
 const inter = Inter({ subsets: ["latin"] });
 
